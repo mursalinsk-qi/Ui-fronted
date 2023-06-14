b3ce861a5b118be09da63746408c17377e8998ab
mursalinsk-qi <mursalin.sk@quantuminventions.com>
2023-06-14 17:47:09 +0530

    'updated'

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import "@qi/map/dist/qimap.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './bootstrap.min.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/pages/TripMap.jsx
+++ src/pages/TripMap.jsx
@@ -1,21 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import axios from "axios";
-import {useParams} from "react-router-dom";
-
+import React from 'react';
+import { QIMapLoader } from "../components/QIMapLoader";
 const TripMap = () => {
-    const [trip, setTrip] = useState({});
-    const {trip_id}=useParams()
-    useEffect(() => {
-        const fetchTrip=async ()=>{
-            const {data} = await axios.get(`http://localhost/v1/data/trips/trip/${trip_id}`)
-            setTrip(data[0].trip_data[0]);
-        }
-       fetchTrip()
-    }, []);
-
     return (
-        <div>
-            <h1>Trip Map Page</h1>
+        <div className='map-container'>
+            <QIMapLoader containerId="device-map-container"/>
         </div>
     );
 };
